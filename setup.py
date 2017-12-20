bafbbd01c7cd6b62856d30e5673eea64432f2420
Steven Engler <smengler3@gmail.com>
2017-12-19 23:35:16 -0500

    Fixed bug in 'setup.py'.
    Fixed a bug in 'setup.py' due to the distutils/setuptools build order and SWIG. The build system was changed from setuptools to distutils.

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
+from distutils.command.build import build
 import glob
+
+# need to reorder the build due to SWIG, see here:
+# https://stackoverflow.com/a/21236111
+
+class CustomBuild(build):
+	sub_commands = [
+	        ('build_ext', build.has_ext_modules),
+	        ('build_py', build.has_pure_modules),
+	        ('build_clib', build.has_c_libraries),
+	        ('build_scripts', build.has_scripts),
+	      ]
 
 example_module = Extension('_newmultimaps',
                            extra_compile_args = ['-Wall', '-Ofast', '-g', '-std=c++11', '-funroll-loops', '-Wfatal-errors'],
@@ -16,6 +28,7 @@ example_module = Extension('_newmultimaps',
 setup(name = 'newmultimaps',
       version = '0.1',
       url = 'https://github.com/stevenengler/new-multilinear-maps',
+      cmdclass={'build': CustomBuild},
       ext_modules = [example_module],
       py_modules = ['newmultimaps'],
      )
